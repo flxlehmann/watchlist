9709f56723185898c62acf9a78b3653b0d223a97
flxlehmann <flxlehmann1@proton.me>
2025-11-07 11:09:10 +0100

    Design Rework

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
 const nextConfig={reactStrictMode:true,experimental:{serverActions:{bodySizeLimit:'2mb'}}};
-nextConfig.images = {
-  remotePatterns: [{ protocol: 'https', hostname: 'image.tmdb.org', pathname: '/t/p/**' }]
-};
 export default nextConfig;
